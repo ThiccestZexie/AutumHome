d38168cc701694a7d08a7167158236d6300c4d98
Daniel Alchasov <danal315@student.liu.se>
2025-04-26 19:47:22 +0200

    updating tag

--- src/app/components/Tag.tsx
+++ src/app/components/Tag.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 
-const Tag = ({ text }) => {
+interface TagProps {
+  text: string;
+}
+
+const Tag: React.FC<TagProps> = ({ text }) => {
   return (
     <div className="transform transition-all duration-100 hover:translate-y-1 inline-block m-2">
       <div className="bg-gray-100 text-gray-700 px-2 py-2 rounded-md text-sm shadow-md hover:shadow-lg font-[family-name:var(--font-jetbrains-mono)]">
